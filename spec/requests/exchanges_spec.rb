913d243ea8605ab7322b56e9b767bd4cbd8e7ac8
Nielson Rolim <nielson.rolim@gmail.com>
2019-06-16 17:23:08 -0300

    Executed VCR to record cassettes

--- spec/requests/exchanges_spec.rb
+++ spec/requests/exchanges_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Exchanges", type: :request, :vcr do
+RSpec.describe "Exchanges", :vcr, type: :request do
 
   describe 'GET #index' do
     it 'returns http success' do
--- spec/support/api_data.rb
+++ spec/support/api_data.rb
@@ -1,76 +1,76 @@
 RSpec.configure do |config|
   config.before(:each) do
 
-    stub_request(:get, /currencydatafeed.com/ )
-    .with(headers: {
-      'Accept'=>'*/*'
-    }).to_return(status: 200, body: '
-    {
-      "status": true,
-      "currency": [
-        {
-          "currency": "USD/BRL",
-          "value": "3.41325",
-          "date": "2018-04-20 17:22:59",
-          "type": "original"
-        }
-      ]
-    }', headers: {})
+    # stub_request(:get, /currencydatafeed.com/ )
+    # .with(headers: {
+    #   'Accept'=>'*/*'
+    # }).to_return(status: 200, body: '
+    # {
+    #   "status": true,
+    #   "currency": [
+    #     {
+    #       "currency": "USD/BRL",
+    #       "value": "3.41325",
+    #       "date": "2018-04-20 17:22:59",
+    #       "type": "original"
+    #     }
+    #   ]
+    # }', headers: {})
 
 
-    stub_request(:get, "https://rest.coinapi.io/v1/exchangerate/BTC/USD").
-    with(
-     headers: {
-      'Accept'=>'application/json',
-      'Accept-Encoding'=>'deflate, gzip',
-      'X-Coinapi-Key'=>Rails.application.credentials[Rails.env.to_sym][:coin_api_key]
-    }).
-    to_return(status: 200, body: '
-    {
-      "time": "2017-08-09T14:31:18.3150000Z",
-      "asset_id_base": "BTC",
-      "asset_id_quote": "USD",
-      "rate": 3260.3514321215056208129867667
-    }', headers: {})
+    # stub_request(:get, "https://rest.coinapi.io/v1/exchangerate/BTC/USD").
+    # with(
+    #  headers: {
+    #   'Accept'=>'application/json',
+    #   'Accept-Encoding'=>'deflate, gzip',
+    #   'X-Coinapi-Key'=>Rails.application.credentials[Rails.env.to_sym][:coin_api_key]
+    # }).
+    # to_return(status: 200, body: '
+    # {
+    #   "time": "2017-08-09T14:31:18.3150000Z",
+    #   "asset_id_base": "BTC",
+    #   "asset_id_quote": "USD",
+    #   "rate": 3260.3514321215056208129867667
+    # }', headers: {})
 
 
-    stub_request(:get, "https://rest.coinapi.io/v1/exchangerate/USD/BRL").
-    with(
-     headers: {
-      'Accept'=>'application/json',
-      'Accept-Encoding'=>'deflate, gzip',
-      'X-Coinapi-Key'=>Rails.application.credentials[Rails.env.to_sym][:coin_api_key]
-    }).
-    to_return(status: 200, body: '
-    {
-      "time": "2017-08-09T14:31:18.3150000Z",
-      "asset_id_base": "USD",
-      "asset_id_quote": "BRL",
-      "rate": 4.005901153995085
-    }', headers: {})
+    # stub_request(:get, "https://rest.coinapi.io/v1/exchangerate/USD/BRL").
+    # with(
+    #  headers: {
+    #   'Accept'=>'application/json',
+    #   'Accept-Encoding'=>'deflate, gzip',
+    #   'X-Coinapi-Key'=>Rails.application.credentials[Rails.env.to_sym][:coin_api_key]
+    # }).
+    # to_return(status: 200, body: '
+    # {
+    #   "time": "2017-08-09T14:31:18.3150000Z",
+    #   "asset_id_base": "USD",
+    #   "asset_id_quote": "BRL",
+    #   "rate": 4.005901153995085
+    # }', headers: {})
 
 
 
-    stub_request(:get, "https://rest.coinapi.io/v1/assets").
-    with(
-     headers: {
-      'Accept'=>'application/json',
-      'Accept-Encoding'=>'deflate, gzip',
-      'X-Coinapi-Key'=>Rails.application.credentials[Rails.env.to_sym][:coin_api_key]
-    }).
-    to_return(status: 200, body: '
-      [
-        {
-          "asset_id": "BTC",
-          "name": "Bitcoin",
-          "type_is_crypto": 1
-        },
-        {
-          "asset_id": "USD",
-          "name": "US Dollar",
-          "type_is_crypto": 0
-        }
-      ]', headers: {})
+    # stub_request(:get, "https://rest.coinapi.io/v1/assets").
+    # with(
+    #  headers: {
+    #   'Accept'=>'application/json',
+    #   'Accept-Encoding'=>'deflate, gzip',
+    #   'X-Coinapi-Key'=>Rails.application.credentials[Rails.env.to_sym][:coin_api_key]
+    # }).
+    # to_return(status: 200, body: '
+    #   [
+    #     {
+    #       "asset_id": "BTC",
+    #       "name": "Bitcoin",
+    #       "type_is_crypto": 1
+    #     },
+    #     {
+    #       "asset_id": "USD",
+    #       "name": "US Dollar",
+    #       "type_is_crypto": 0
+    #     }
+    #   ]', headers: {})
 
   end
 end
