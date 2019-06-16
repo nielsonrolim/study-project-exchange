477e89e54e4f1032e6011518cede3d0f0777323f
Nielson Rolim <nielson.rolim@gmail.com>
2019-06-16 17:17:25 -0300

    Added VCR to Exchanges Spec

--- spec/requests/exchanges_spec.rb
+++ spec/requests/exchanges_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Exchanges", type: :request do
+RSpec.describe "Exchanges", type: :request, :vcr do
 
   describe 'GET #index' do
     it 'returns http success' do
